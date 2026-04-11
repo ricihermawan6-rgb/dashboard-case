5f5a3b39db206b19d6ea3a182ef1e927cb3cb3a6
ricihermawan6-rgb <ricihermawan6@gmail.com>
2026-04-12 02:24:54 +0700

    Delete casetrack directory

--- casetrack/app/bak/layout.tsx
+++
@@ -1,4 +0,0 @@
-import DashboardLayout from '@/app/dashboard/layout'
-export default function BAKLayout({ children }: { children: React.ReactNode }) {
-  return <DashboardLayout>{children}</DashboardLayout>
-}
